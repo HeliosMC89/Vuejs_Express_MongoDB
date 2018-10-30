d58eb6a6544a48bd03365a2d2ce7e08e24e8e64c
Heliodoro Morales Cabrera <moralescabrera.heliodoro@gmail.com>
2018-10-30 11:44:46 -0400

    frontendagregado la carpeta public y SPA.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const cors = require('cors');
 
 const app = express();
@@ -14,6 +15,16 @@ const posts = require('./routes/api/posts');
 // routes
 app.use('/api/posts/', posts);
 
+// handle production
+if (process.env.NODE_ENV) {
+  // static folder
+  app.use(express.static(path.resolve(__dirname, 'public')));
+
+  // Handle SPA
+  app.get(/.*/, (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  });
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
